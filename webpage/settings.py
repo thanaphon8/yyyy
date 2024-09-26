eef2bad6722211ae76a4646c7e2e872844f9522a
Thanaphon <dthanaphon21@gmail.com>
2024-09-26 13:23:40 +0700

    commit

--- webpage/settings.py
+++ webpage/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-as#gqylj^6knf*gw@#w&ea=jr5w+*k1mypp$o8$kjt5w9g#@@y'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
 
